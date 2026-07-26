3ad1e3a4d1b60458a27123d6670b599c857070e9
Mathurin Maussion <mathurinmaus@gmail.com>
2026-07-27 00:38:24 +0200

    Add Terraform CI GitHub Actions workflow

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,8 +1,18 @@
 terraform {
+  required_version = ">= 1.15.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket       = "cloud-automation-lab-tfstate-mathurin"
+    key          = "cloud-automation-lab/terraform.tfstate"
+    region       = "eu-west-3"
+    encrypt      = true
+    use_lockfile = true
   }
 }
